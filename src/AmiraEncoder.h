021db7aa0cbcbe70739ccd5eeb5c49da643cbe92
Livio Bellini <bellinilivio90@gmail.com>
2025-11-29 06:02:30 +0100

    Update AmiraEncoder.h
    Improved interaction with hardware.

--- src/AmiraEncoder.h
+++ src/AmiraEncoder.h
@@ -5,14 +5,29 @@
 
 #define INTERNAL 2
 #define EXTERNAL 3
-#define DIR_NONE 0x00
+#define DIR_NONE 0x0
 #define DIR_CW   0x10
 #define DIR_CCW  0x20
+#define R_START  0x0
+
+#ifdef HALF_STEP								      //Use the half-step state table (emits a code at 00 and 11).
+#define R_CCW_BEGIN 0x1
+#define R_CW_BEGIN 0x2
+#define R_START_M 0x3
+#define R_CW_BEGIN_M 0x4
+#define R_CCW_BEGIN_M 0x5
+#else										              //Use the full-step state table (emits a code at 00 only).
+#define R_CW_FINAL 0x1
+#define R_CW_BEGIN 0x2
+#define R_CW_NEXT 0x3
+#define R_CCW_BEGIN 0x4
+#define R_CCW_FINAL 0x5
+#define R_CCW_NEXT 0x6
+#endif
 
 class Encoder {
   public:
     Encoder(uint8_t _pinA, uint8_t _pinB, uint8_t _pullup = INTERNAL, byte _encSens = 0);
-    void begin();
     int loop(int _value);
     uint8_t setStep(uint8_t _normStep);
     uint8_t setAccel(uint8_t _longStep);
