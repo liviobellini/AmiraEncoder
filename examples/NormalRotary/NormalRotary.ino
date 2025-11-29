f024c10c6fc393c20b2f54c92e937b7c273806b2
Livio Bellini <bellinilivio90@gmail.com>
2025-11-29 04:03:43 +0100

    Update NormalRotary.ino

--- examples/NormalRotary/NormalRotary.ino
+++ examples/NormalRotary/NormalRotary.ino
@@ -18,8 +18,8 @@ void setup() {
   lcd.begin(16, 2);                                                          //Initialize the LCD display with 16 columns and 2 rows.
   lcd.setBacklight(255);                                                     //Turn on lcd backlight.
   myEncoder.begin();                                                         //Execute setup for rotary encoder and read the state.
-  //IMPORTANT: if you don't call setStep then default values will be 1.
-  //IMPORTANT: if you don't call setAccel then default values will be 1.
+  //IMPORTANT: if you don't call setStep then default value will be 1.
+  //IMPORTANT: if you don't call setAccel then default value will be 1.
   lcd.clear();
   lcd.home();
   lcd.print("VOLUME:    db");
