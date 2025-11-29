4738a9c6da6ab89e5daf96b09229e4a972f445ad
Livio Bellini <bellinilivio90@gmail.com>
2025-11-29 06:09:13 +0100

    Update AcceleratedRotary.ino
    Improved AcceleratedRotary example.

--- examples/AcceleratedRotary/AcceleratedRotary.ino
+++ examples/AcceleratedRotary/AcceleratedRotary.ino
@@ -1,6 +1,7 @@
 //AmiraEncoder accelerated rotary encoder.
 //This example shows how to use a rotary encoder with accelerated increment, enjoy. :-)
 #include <Arduino.h>
+//#define HALF_STEP                                                          //Uncomment if you want to use half step rotary encoder table.
 #include <AmiraEncoder.h>
 #include <Wire.h>
 #include <hd44780.h>
